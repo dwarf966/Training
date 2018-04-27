9aaf7108d375d393e58c89fc3843798c6865c501
dwarf966 <ikkunk66@yahoo.co.jp>
2018-04-27 21:27:56 +0900

    Initial Commit

---
+++ Training/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Training
+//
+//  Created by 内藤生真 on 2018/04/27.
+//  Copyright © 2018年 内藤生真. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
